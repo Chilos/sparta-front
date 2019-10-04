99559821378bfde8e147ea602e713835ae7135e6
Chilos Ohh <chilos.ohh@yandex.ru>
2019-10-04 18:42:16 +0300

    Пилим материал

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './shared/components/auth-layout/auth-layout.component';
+import { UsersPageComponent } from './users-page/users-page.component';
+import { OptionsPageComponent } from './options-page/options-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+    {path: '', redirectTo: '/users', pathMatch: 'full'},
+    {path: 'users', component: UsersPageComponent},
+    {path: 'options', component: OptionsPageComponent},
+
+  ]},
+  {path: '', component: AuthLayoutComponent, children: [
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'login', component: LoginPageComponent }
+  ]},
+  {path: '**', redirectTo: 'login'}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,6 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 
-    constructor(private router: Router) {
-      router.navigateByUrl('login');
+    constructor() {
     }
 }
